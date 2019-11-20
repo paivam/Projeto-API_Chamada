fd3f6cb363fd5fbb5382c093c6cca044ecae1563
souzaMateus99 <mateussam.souza@gmail.com>
2019-11-20 16:14:16 -0300

    Include Repository annotation in repo. classes

--- src/main/java/br/com/chamada/repository/UsuarioRepository.java
+++ src/main/java/br/com/chamada/repository/UsuarioRepository.java
@@ -2,8 +2,10 @@ package br.com.chamada.repository;
 
 import br.com.chamada.model.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import java.util.Optional;
 
+@Repository
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
     Optional<Usuario> findByLoginAndSenha(String login, String senha);
 }
