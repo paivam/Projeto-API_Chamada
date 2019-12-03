f139a2bf24de9e9013635d11b3fa8b436bf3ea8e
Mateus Sampaio <mateussam.souza@gmail.com>
2019-12-03 02:32:46 -0300

    Alter path to config package

--- src/main/java/br/com/chamada/config/WebConfig.java
+++ src/main/java/br/com/chamada/config/WebConfig.java
@@ -1,4 +1,4 @@
-package br.com.chamada.controller.config;
+package br.com.chamada.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
